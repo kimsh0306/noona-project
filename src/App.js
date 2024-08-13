8b60f5d68e41c5b0a7c519ead40546cc009a8718
kimsh0306 <b666790@gmail.com>
2024-08-13 22:54:44 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Box from "./component/Box"
 
+const choice = {
+  rock: {
+    name: "Rock",
+    img: "https://cdn-icons-png.flaticon.com/512/5773/5773204.png"
+  },
+  scissors: {
+    name: "Scissors",
+    img: "https://cdn-icons-png.flaticon.com/512/4975/4975327.png"
+  },
+  paper: {
+    name: "Paper",
+    img: "https://cdn-icons-png.flaticon.com/512/2541/2541988.png"
+  }
+}
 function App() {
+  const [userSelect, setUserSelect] = useState(null);
+
+  const play = (userChoice) => {
+    console.log("선택됨!", userChoice)
+    setUserSelect(choice[userChoice]);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='main'>
+        <Box title="you" item={userSelect} />
+        {/* <Box title="computer" /> */}
+      </div>
+      <div className='main'>
+        <button onClick={() => play("scissors")}>가위</button>
+        <button onClick={() => play("rock")}>바위</button>
+        <button onClick={() => play("paper")}>보</button>
+      </div>
     </div>
   );
 }
